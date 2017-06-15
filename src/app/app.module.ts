38700b01a14235846324a5a7048caa17ce321228
Anton Kharitonov <anton.kharitonoff@gmail.com>
2017-06-15 15:18:08 +0200

    add logo; switch to angular4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MomentModule } from 'angular2-moment';
+// import { MomentModule } from 'angular2-moment';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { routing } from './app.routes';
-import { TooltipModule, AlertModule, ButtonsModule, DropdownModule } from 'ng2-bootstrap';
-import { Ng2PaginationModule } from 'ng2-pagination';
-import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { TooltipModule, AlertModule, ButtonsModule, BsDropdownModule } from 'ng2-bootstrap';
+// import { Ng2PaginationModule } from 'ng2-pagination';
+// import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 // components
 import { AppComponent } from './app.component';
@@ -32,18 +33,19 @@ import { MouseWheelDirective } from './directives/mousewheel.directive';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpModule,
         JsonpModule,
-        MomentModule,
+        // MomentModule,
         FormsModule,
         ReactiveFormsModule,
         routing,
         TooltipModule,
-        Ng2PaginationModule,
+        // Ng2PaginationModule,
         AlertModule,
         ButtonsModule,
-        DropdownModule.forRoot(),
-        Ng2PageScrollModule.forRoot()
+        BsDropdownModule.forRoot()
+        // Ng2PageScrollModule.forRoot()
     ],
     providers: [
     ],
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,12 +7,19 @@ import { Component } from '@angular/core';
 export class AboutComponent {
     content = [{
         title: 'ABOUT ME',
-        imageUrl: '/assets/about1.jpg',
-        description: `“When people look at my pictures I want them to feel the way they do when they want to read a line of a poem twice.” Robert Frank`,
+        imageUrl: 'assets/about1.jpg',
+        // description: `“When people look at my pictures I want them to feel the way they do when they want to read a line of a poem twice.” Robert Frank`,
+        description: `Hallo,<br><br>
+schön, dass DU hier bist!<br>
+Ich bin Katrin und die Fotografie ist meine große Leidenschaft! Sie ist für mich eine Hommage an das Leben, die Liebe, das Lachen...
+Gefühlvoll und mit Liebe zum Detail, erzähle ich mit meinen Bildern visuelle Geschichten von der Liebe zweier Menschen, von freudiger Erwartung auf neues Leben, vom Zauber der ersten Tage, von strahlenden Kinderaugen, vom bunten Miteinander einer Familie... So bleiben diese besonderen Momente für immer lebendig.
+Lass dich von meinen Bildern inspirieren, vielleicht wird die nächste Geschichte deine ganz persönliche!<br><br>
+Schreib mir gerne eine E-Mail, ich freue mich auf dich!<br><br>
+Katrin`,
         navigate: 1
     }, {
         title: 'MORE ABOUT ME',
-        imageUrl: '/assets/about2.jpg',
+        imageUrl: 'assets/about2.jpg',
         description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
                     et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum`
     }]
