e1f8fe9be4a3cd298daf26e5fd6615b68ff05c86
Anton Kharitonov <anton.kharitonoff@gmail.com>
2017-06-15 23:39:45 +0200

    fix deploy script

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,7 +11,7 @@ export class AboutComponent {
         // description: `“When people look at my pictures I want them to feel the way they do when they want to read a line of a poem twice.” Robert Frank`,
         description: `Hallo,<br><br>
 schön, dass DU hier bist!<br>
-Ich bin Katrin und die Fotografie ist meine große Leidenschaft! Sie ist für mich eine Hommage an das Leben, die Liebe, das Lachen...
+Ich bin Katrin und die Fotografie ist meine große Leidenschaft! Sie ist für mich eine Hommage an das Leben, die Liebe, das Lachen...<br>
 Gefühlvoll und mit Liebe zum Detail, erzähle ich mit meinen Bildern visuelle Geschichten von der Liebe zweier Menschen, von freudiger Erwartung auf neues Leben, vom Zauber der ersten Tage, von strahlenden Kinderaugen, vom bunten Miteinander einer Familie... So bleiben diese besonderen Momente für immer lebendig.
 Lass dich von meinen Bildern inspirieren, vielleicht wird die nächste Geschichte deine ganz persönliche!<br><br>
 Schreib mir gerne eine E-Mail, ich freue mich auf dich!<br><br>
