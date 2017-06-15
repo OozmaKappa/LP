092b3735646236f0b067b1b8e3b93af7c58e1b9d
Anton Kharitonov <anton.kharitonoff@gmail.com>
2017-06-15 15:29:33 +0200

    fix animation import

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, trigger, state, style, transition, animate } from '@angular/core';
+import { Component} from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 
 function fadeInOut() {
