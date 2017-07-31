3cf0af13fd106833d10b22dba6557adcb3a62464
Anton Kharitonov <anton.kharitonoff@gmail.com>
2017-07-31 19:44:16 +0200

    notready page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component, ViewContainerRef } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app-lp',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
     private viewContainerRef: ViewContainerRef;
+    host: string;
 
     public constructor(viewContainerRef: ViewContainerRef) {
         // ng2-bootstrap modal hack
         this.viewContainerRef = viewContainerRef;
+        this.host = window.location.hostname;
     }
-
 };
